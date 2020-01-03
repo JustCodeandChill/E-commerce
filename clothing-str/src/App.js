f7cb7fe661ff79f6635a624896dd19ac1f2a4f3e
JustCodeandChill <inforreceiver@gmail.com>
2020-01-03 16:02:17 -0600

    Update menu-items to navigate withRouter HOC

--- clothing-str/src/App.js
+++ clothing-str/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+      <Route  path='/' component={HomePage}></Route>
+      </Switch>
     </div>
   );
 }
