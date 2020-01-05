7be2cd8761d2e29d2cf297e288ea5cf9e1985e00
JustCodeandChill <inforreceiver@gmail.com>
2020-01-04 22:31:44 -0600

    Adding header component and lunk to home, shop , contact

--- clothing-str/src/App.js
+++ clothing-str/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
@@ -9,6 +9,7 @@ import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
