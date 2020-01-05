98e0be96d6700bb649386c5067480b057683710d
JustCodeandChill <inforreceiver@gmail.com>
2020-01-04 22:05:11 -0600

    Update app with a shop page: collection-preview component and collection-item component

--- clothing-str/src/App.js
+++ clothing-str/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
-import logo from './logo.svg';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <div >
       <Switch>
-      <Route  path='/' component={HomePage}></Route>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
